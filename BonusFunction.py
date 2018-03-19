040de825d6b581925c22886c39e6281c4117b340
Shyam Uthaman <37495389+shyamuthaman@users.noreply.github.com>
2018-03-18 18:40:51 -0700

    Add files via upload

---
+++ BonusFunction.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar 18 17:56:28 2018
+
+@author: shyam
+"""
+
+def bonusCalc(salary,country):
+    bonus = salary * 0.1
+    print(country)
+    return(bonus)
